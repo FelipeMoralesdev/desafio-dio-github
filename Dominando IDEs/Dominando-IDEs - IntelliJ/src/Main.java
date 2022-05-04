aa0560b717a961a7249744c12d921e4653f55f8b
Felipe Morales <fmoralessimoes@gmail.com>
2022-05-04 20:34:57 -0300

    Organizando pastas

---
+++ Dominando IDEs/Dominando-IDEs - IntelliJ/src/Main.java
@@ -0,0 +1,12 @@
+import model.Gato;
+
+public class Main {
+    public static void main(String[] args) {
+
+        /*        System.out.println("Olá Mundo");*/
+
+        Gato gato = new Gato("Agata", "Tartaruga", 8);
+
+        System.out.println(gato);
+    }
+}
